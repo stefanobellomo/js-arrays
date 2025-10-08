5cab6c5981cd7712e4d279f693bd3ee9f16fd443
stefanobellomo <stefanobellomo1998@gmail.com>
2025-10-08 15:37:23 +0200

    ex 4 bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -35,7 +35,8 @@ const ed_index = teachers.indexOf('Ed')
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.includes('Fabio');
+console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
