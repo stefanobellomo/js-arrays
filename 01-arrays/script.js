bf7b205fe966d80944d04b14cb24c46fc1a9008b
stefanobellomo <stefanobellomo1998@gmail.com>
2025-10-08 14:26:35 +0200

    ex 3, 4, 5

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -19,12 +19,16 @@ teachers.splice(4, 1, 'Patrick');
 console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop();
+console.log(lastTeacher);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
+console.log(firstTeacher);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push('Vanessa');
+console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
