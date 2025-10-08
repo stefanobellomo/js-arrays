c17baf87145ef6336f7f81d36990befb94e58f18
stefanobellomo <stefanobellomo1998@gmail.com>
2025-10-08 17:37:40 +0200

    fix ex 2

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -15,7 +15,7 @@ console.log(fourthTeacher);
 
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-teachers.splice(4, 1, 'Patrick');
+teachers[4] = 'Patrick';
 console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
