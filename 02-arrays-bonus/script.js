72ba92f9c63fd7c4f34835683ac30b9341fd23f8
stefanobellomo <stefanobellomo1998@gmail.com>
2025-10-08 15:33:20 +0200

    ex 3 bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -25,6 +25,13 @@ for (let i = 0; i < teachers.length; i++) {
 console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+// teachers.splice(1, 1)
+// console.log(teachers);
+const ed_index = teachers.indexOf('Ed')
+  if (ed_index > -1){
+    teachers.splice(ed_index, 1)
+  }
+  console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
